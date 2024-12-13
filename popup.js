08430e0ccab6e0de85035cd950c9e6ad67bd1f80
Ashaan Dalgliesh <140295753+fivepandasna@users.noreply.github.com>
2024-12-13 13:43:26 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("start").addEventListener("click", () => {
-  chrome.runtime.getBackgroundPage((backgroundPage) => {
-    backgroundPage.inspecting = true;
-  });
-  window.close();
-});
